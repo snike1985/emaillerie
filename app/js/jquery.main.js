93216f582ee3d814ac370ab1268f3c25fc488f13
alexserga <snike1985@gmail.com>
2017-10-25 15:31:19 +0300

    appartaments functional doing

--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -4,7 +4,7 @@
     $(function(){
 
         $('.appartment').each( function() {
-            new Appartment( $(this) );
+            // new Appartment( $(this) );
         } );
 
         $('.callback').each( function() {
@@ -35,8 +35,16 @@
             new Steps( $(this) );
         } );
 
+        $('.storey').each( function() {
+            new Storey( $(this) );
+        } );
+
         $('.show').each( function() {
             new Show( $(this) );
+        } );
+
+        $('.plan').each( function() {
+            new Plan( $(this) );
         } );
 
     });
@@ -593,10 +601,6 @@
             _window = $( window ),
             _storey = document.getElementById('storey'),
             _ctxStorey = _storey.getContext('2d'),
-            _ctxStorey1 = _storey.getContext('2d'),
-            _ctxStorey2 = _storey.getContext('2d'),
-            _ctxStorey3 = _storey.getContext('2d'),
-            _ctxStorey4 = _storey.getContext('2d'),
             _build = document.getElementById('build'),
             _ctxBuild = _build.getContext('2d'),
             _imgBuild = new Image(),
@@ -664,10 +668,7 @@
             _openStorey = function () {
 
                 if (_activeStorey) {
-                    console.log('Storey ', _activeStorey);
-                    console.log($('.steps__storey').length);
-                    $('.steps__storey').removeClass('active');
-                    $('.steps__storey').eq(_activeStorey - 1).addClass('active');
+                    $('.storey')[0].obj.draw(_activeStorey - 1);
                 }
             },
             _dawBuild = function() {
@@ -701,36 +702,16 @@
         var _self = this,
             _obj = obj,
             _item = _obj.find('.appartment__item'),
-            _window = $( window ),
-            _storey = document.getElementById('storey1'),
-            _ctxStorey = _storey.getContext('2d'),
-            _build = document.getElementById('appartment'),
-            _ctxBuild = _build.getContext('2d'),
-            _imgBuild = new Image(),
+            _appartment = document.createElement('canvas'),
+            _ctxAppartment = _appartment.getContext('2d'),
             _mouseX = 0,
             _mouseY = 0,
-            _activeStorey = 0;
-
-        _storey.width = 1811;
-        _storey.height = 700;
-
+            _activeFlat = 0;
+        
         //private methods
         var _addEvents = function() {
 
-                _window.on( {
-                    'load': function() {
-                        _dawBuild();
-                    }
-                } );
-
-                _obj.on( {
-                    'mousemove': function(e) {
-
-
-                    }
-                } );
-
-                _storey.addEventListener('mousemove',function (e) {
+                _appartment.addEventListener('mousemove',function (e) {
                     var koef = 1811/$(this).width(),
                         elemTop = $(this).offset().top,
                         elemLeft = $(this).offset().left;
@@ -742,8 +723,12 @@
 
                     _updateCanvas();
                 },false);
-                _storey.addEventListener('click',_openStorey,false);
 
+            },
+            _createCanvas = function () {
+                _obj.prepend(_appartment);
+                _appartment.width = 1811;
+                _appartment.height = 700;
             },
             _drawCanvas = function () {
 
@@ -751,54 +736,81 @@
                     var curElem = $(this),
                         path = curElem.data('path');
 
-                    _ctxStorey.beginPath();
-                    _ctxStorey.moveTo(path[0][0],path[0][1]);
+                    _ctxAppartment.beginPath();
+                    _ctxAppartment.moveTo(path[0][0],path[0][1]);
                     for (var i = 1; i < path.length; i++) {
-                        _ctxStorey.lineTo(path[i][0],path[i][1]);
+                        _ctxAppartment.lineTo(path[i][0],path[i][1]);
                     }
-                    _ctxStorey.globalAlpha = 0;
-                    if (_ctxStorey.isPointInPath(_mouseX, _mouseY)) {
-                        // TweenMax.to(_ctxStorey, 1 ,{
-                        //     globalAlpha: 1
-                        // });
-                        _ctxStorey.globalAlpha = 1;
-                        _activeStorey = y + 1;
+                    _ctxAppartment.globalAlpha = 0;
+                    if (_ctxAppartment.isPointInPath(_mouseX, _mouseY)) {
+                        _ctxAppartment.globalAlpha = 1;
+                        _activeFlat = y + 1;
                     }
-                    _ctxStorey.fillStyle = 'rgba(255,78,0,.4)';
-                    _ctxStorey.fill();
-                    _ctxStorey.closePath();
-                    _ctxStorey.restore();
+                    _ctxAppartment.fillStyle = 'rgba(255,78,0,.4)';
+                    _ctxAppartment.fill();
+                    _ctxAppartment.closePath();
+                    _ctxAppartment.restore();
                 });
             },
             _updateCanvas = function () {
-                _ctxStorey.clearRect(0,0,_storey.width,_storey.height);
-                _activeStorey = null;
+                _ctxAppartment.clearRect(0,0,_appartment.width,_appartment.height);
+                _activeFlat = null;
                 _drawCanvas();
-            },
-            _openStorey = function () {
-
-            },
-            _dawBuild = function() {
-
-                _imgBuild.src = 'img/storeys/storey1.jpg';
-
-                _imgBuild.onload = function() {
-                    _ctxBuild.canvas.width = _imgBuild.width;
-                    _ctxBuild.canvas.height = _imgBuild.height;
-                    _ctxBuild.drawImage(_imgBuild,0,0);
-
-                    // _drawStorey();
-                }
-
             },
             _init = function() {
                 _obj[ 0 ].obj = _self;
                 _addEvents();
+                _createCanvas();
             };
 
         //public properties
 
         //public methods
+
+        _init();
+    };
+
+    var Storey = function(obj) {
+
+        //private properties
+        var _self = this,
+            _obj = obj,
+            _item = _obj.find('.storey__item'),
+            _activeItem = _obj.find('.storey__item').eq(0),
+            _imgStorey = new Image(),
+            _storey = document.createElement('canvas'),
+            _ctxStorey = _storey.getContext('2d');
+        
+        //private methods
+        var _addEvents = function() {
+
+            },
+            _drawStorey = function() {
+                _imgStorey.src = _activeItem.data('img');
+                _imgStorey.onload = function() {
+                    _ctxStorey.drawImage(_imgStorey,0,0);
+                };
+
+                new Appartment(_activeItem.find('.appartment'));
+            },
+            _createCanvas = function () {
+                _obj.prepend(_storey);
+                _storey.width = 1811;
+                _storey.height = 700;
+            },
+            _init = function() {
+                _obj[ 0 ].obj = _self;
+                _addEvents();
+                _createCanvas();
+            };
+
+        //public properties
+
+        //public methods
+        _self.draw = function (activeStoreyIndex) {
+            _activeItem = _item.eq(activeStoreyIndex);
+            _drawStorey();
+        };
 
         _init();
     };
@@ -864,6 +876,207 @@
                 _onEvents();
                 _checkScroll();
                 // _startAnimation();
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
+
+    var Plan = function(obj) {
+
+        //private properties
+        var _self = this,
+            _obj = obj,
+            _wrap = _obj.find('.plan__wrap'),
+            _back = _obj.find('.plan__back'),
+            _build = _obj.find('.plan__build'),
+            _floors = _obj.find('.plan__floor'),
+            _appartments = _obj.find('.plan__appartment'),
+
+            _buildFloorCanvas = document.createElement('canvas'),
+            _buildFloorCanvasCtx = _buildFloorCanvas.getContext('2d'),
+
+            _floorCanvas = document.createElement('canvas'),
+            _floorCanvasCtx = _floorCanvas.getContext('2d'),
+
+            _mouseX = 0,
+            _mouseY = 0,
+            _activeFloor = null,
+            _activeAppartment = null,
+            _step = 0;
+
+        _floorCanvas.classList.add('floor');
+
+        //private methods
+        var _addEvents = function() {
+                _wrap.on( {
+                    'mousemove': function(e) {
+                        var koef = 1811/$(this).width(),
+                            elemTop = $(this).offset().top,
+                            elemLeft = $(this).offset().left;
+
+                        _mouseX = (e.pageX - elemLeft)*koef;
+                        _mouseY = (e.pageY - elemTop)*koef;
+
+                        switch (_step) {
+                            case 0:
+                                _updateBuildFloor();
+                                break;
+                            case 1:
+                                _updateFloor();
+                                break;
+                            case 2:
+                                break;
+                            default:
+                                break;
+                        }
+                    },
+                    'click': function() {
+
+                        switch (_step) {
+                            case 0:
+                                _openFloor();
+                                break;
+                            case 1:
+                                _openAppartment();
+                                break;
+                            case 2:
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                } );
+
+                _back.on({
+                    'click': function () {
+
+                        _step = _step - 1;
+
+                        switch (_step) {
+                            case 0:
+                                _back.addClass('hide');
+                                $('.plan__floor').removeClass('active');
+                                _updateBuildFloor();
+                                break;
+                            case 1:
+                                $('.plan__appartment').removeClass('active');
+                                _openFloor();
+                                _updateFloor();
+                                break;
+                            case 2:
+                                _openAppartment();
+                                break;
+                            default:
+                                break;
+                        }
+                        return false;
+                    }
+                });
+            },
+            _createBackground = function (elem) {
+                elem.each(function () {
+                    var curElem = $(this);
+
+                    curElem.css({ 'background-image': 'url(' + curElem.data('img') + '' });
+                });
+            },
+            _createBuildFloorCanvas = function () {
+                _build.prepend(_buildFloorCanvas);
+                _buildFloorCanvas.width = 1811;
+                _buildFloorCanvas.height = 700;
+                _drawBuildFloor();
+            },
+            _createFloorCanvas = function () {
+                _build.prepend(_floorCanvas);
+                _floorCanvas.width = 1811;
+                _floorCanvas.height = 700;
+                _drawFloor();
+            },
+            _drawBuildFloor = function () {
+
+                _floors.each(function (y) {
+                    var curElem = $(this),
+                        path = curElem.data('path');
+
+                    _buildFloorCanvasCtx.beginPath();
+                    _buildFloorCanvasCtx.moveTo(path[0][0],path[0][1]);
+                    for (var i = 1; i < path.length; i++) {
+                        _buildFloorCanvasCtx.lineTo(path[i][0],path[i][1]);
+                    }
+                    _buildFloorCanvasCtx.globalAlpha = 0;
+                    if (_buildFloorCanvasCtx.isPointInPath(_mouseX, _mouseY)) {
+                        _buildFloorCanvasCtx.globalAlpha = 1;
+                        _activeFloor = y;
+                    }
+                    _buildFloorCanvasCtx.fillStyle = 'rgba(255,78,0,.4)';
+                    _buildFloorCanvasCtx.fill();
+                    _buildFloorCanvasCtx.closePath();
+                    _buildFloorCanvasCtx.restore();
+                });
+            },
+            _updateBuildFloor = function () {
+                _buildFloorCanvasCtx.clearRect(0,0,_buildFloorCanvas.width,_buildFloorCanvas.height);
+                _activeFloor = null;
+                _drawBuildFloor();
+            },
+            _drawFloor = function () {
+
+                _floors.eq(_activeFloor).find('.plan__appartment').each(function (y) {
+                    var curElem = $(this),
+                        path = curElem.data('path');
+
+                    _floorCanvasCtx.beginPath();
+                    _floorCanvasCtx.moveTo(path[0][0],path[0][1]);
+                    for (var i = 1; i < path.length; i++) {
+                        _floorCanvasCtx.lineTo(path[i][0],path[i][1]);
+                    }
+                    _floorCanvasCtx.globalAlpha = 0;
+                    if (_floorCanvasCtx.isPointInPath(_mouseX, _mouseY)) {
+                        _floorCanvasCtx.globalAlpha = 1;
+                        _activeAppartment = y;
+                    }
+                    _floorCanvasCtx.fillStyle = 'rgba(255,78,0,.4)';
+                    _floorCanvasCtx.fill();
+                    _floorCanvasCtx.closePath();
+                    _floorCanvasCtx.restore();
+                });
+            },
+            _updateFloor = function () {
+                _floorCanvasCtx.clearRect(0,0,_floorCanvas.width,_floorCanvas.height);
+                _activeAppartment = null;
+                _drawFloor();
+            },
+            _openFloor = function () {
+
+                if (_activeFloor > -1) {
+                    _floors.eq(_activeFloor).addClass('active');
+                }
+
+                _step = 1;
+                _back.removeClass('hide');
+            },
+            _openAppartment = function () {
+                _floors.eq(_activeFloor).find('.plan__appartment').eq(_activeAppartment).addClass('active');
+                console.log('open appartament C' + _activeFloor + '.' + (_activeAppartment + 1 ));
+                _mouseX = 0;
+                _mouseY = 0;
+                _step = 2;
+                _updateFloor();
+            },
+            _init = function() {
+                _obj[ 0 ].obj = _self;
+                _addEvents();
+
+                _createBackground(_build);
+                _createBackground(_floors);
+                _createBackground(_appartments);
+
+                _createFloorCanvas();
+                _createBuildFloorCanvas();
             };
 
         //public properties
